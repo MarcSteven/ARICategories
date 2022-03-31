e0c3cc866c665c982beb82f8518b50df069dee03
marc steven <zhaoxinqiang328@163.com>
2022-03-31 09:15:09 +0800

    remove unused methods

--- ARICategoryKit/Source/UIKit/UIViewControllerExtension.swift
+++ ARICategoryKit/Source/UIKit/UIViewControllerExtension.swift
@@ -35,19 +35,7 @@ public  extension UIViewController {
     }
 }
 public extension  UIViewController {
-    func install(_ child:UIViewController) {
-        addChild(child)
-        child.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(child.view)
-        NSLayoutConstraint.activate([
-            child.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            child.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            child.view.topAnchor.constraint(equalTo: view.topAnchor),
-            child.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        child.didMove(toParent: self)
-    }
-   
+    
     /// Removes the view controller from its parent.
      func removeFromContainerViewController() {
         guard parent != nil else {
@@ -131,36 +119,8 @@ public extension  UIViewController {
     }
 
 
-    func addFullScreen(childViewController child:UIViewController) {
-        guard child.parent == nil else {
-            return
-        }
-        addChild(child)
-        view.addSubview(child.view)
-        
-        child.view.translatesAutoresizingMaskIntoConstraints = false
-        let constraints = [
-            view.leadingAnchor.constraint(equalTo: child.view.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: child.view.trailingAnchor),
-            view.topAnchor.constraint(equalTo: child.view.topAnchor),
-            view.bottomAnchor.constraint(equalTo: child.view.bottomAnchor)
-        ]
-        constraints.forEach {$0.isActive = true}
-        view.addConstraints(constraints)
-        
-        child.didMove(toParent: self)
-    }
-    func remove(childViewController child:UIViewController?) {
-        guard let child = child else {
-            return
-        }
-        guard child.parent != nil else {
-            return
-        }
-        child.willMove(toParent: nil)
-        child.view.removeFromSuperview()
-        child.removeFromParent()
-    }
+    
+    
 }
 
 public extension UIViewController {
